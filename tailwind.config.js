1847f6405d53e73647da1966add4438de2dc35fc
dloyolar <dieegolr@gmail.com>
2022-02-19 13:44:33 -0300

    [REFACTOR] 📝 remove deprecated config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*{html,js}"],
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },

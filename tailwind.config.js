d24ac652ed3155fc3a401cbec047f3b89ce0084a
dloyolar <dieegolr@gmail.com>
2022-02-19 12:29:07 -0300

    [CHORE] ✨add tailwind and postcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*{html,js}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

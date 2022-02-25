fd6d226a9021e03d65d5867071e0e2da80237b0d
dloyolar <dieegolr@gmail.com>
2022-02-25 13:28:21 -0300

    [ADD] ✨ SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import "@styles/tailwind.css";
 import MainLayout from "@layout/MainLayout";
 import { ProviderAuth } from "@hooks/useAuth";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ProviderAuth>
+      <Head>
+        <title>Next Shop Admin</title>
+      </Head>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
